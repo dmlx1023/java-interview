b61e2d0c563ee913f845e7ccb3f9a51fd9a078db
limuyan44 <1332729132@qq.com>
2020-07-29 12:15:24 +0800

    cors

--- csrf/src/main/java/com/zycat/csrf/CSRFController.java
+++ csrf/src/main/java/com/zycat/csrf/CSRFController.java
@@ -1,14 +1,22 @@
 package com.zycat.csrf;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CSRFController {
     @RequestMapping("h")
+    @CrossOrigin
     public String h(){
         System.out.println("一个请求进入了啊");
         return "h";
 
+    }
+    @RequestMapping("h1")
+    public String jsonp(){
+        JSONPObject json = new JSONPObject("random",Math.random());
+        return json.toString();
     }
 }
