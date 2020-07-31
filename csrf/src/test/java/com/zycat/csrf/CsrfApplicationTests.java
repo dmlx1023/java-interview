3d7825676011d7589396ed9e2e60c7d15a152428
limuyan44 <1332729132@qq.com>
2020-07-31 22:18:40 +0800

    t

--- csrf/src/test/java/com/zycat/csrf/CsrfApplicationTests.java
+++ csrf/src/test/java/com/zycat/csrf/CsrfApplicationTests.java
@@ -2,6 +2,7 @@ package com.zycat.csrf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjuster;
@@ -42,6 +43,19 @@ class CsrfApplicationTests {
             System.out.printf(nowDate.getYear() + "年%02d月%02d日开心\n", nowDate.getMonthValue(), nowDate.getDayOfMonth());
             nowDate = nowDate.plusDays(1);
         }
+        Assert.isTrue(偶数(2));
     }
 
+    private boolean 偶数(int 输入值){
+        for(int i=0;i<=Integer.MAX_VALUE;i++){
+//            如果i是偶数切 输入值和i相等，说明输入值也是偶数
+            if (i%2==0&&输入值==i){
+                return true;
+            }else {
+                return false;
+            }
+
+        }
+        return false;
+    }
 }
